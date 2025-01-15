11a6b2abff74b6083dd715f09c6a62c9f47a310e
noobieplayer <1youichiyo@gmail.com>
2025-01-15 16:13:13 +0900

    update image_processing.py

--- function/image_processing.py
+++ function/image_processing.py
@@ -11,26 +11,65 @@ import numpy as np
 # メイン関数
 def main():
     # 画像読み込み
-    image_name = "./img/queue_gpt.jpg"
-    img = cv2.imread(image_name)
+    image_path = "./img/queue_gpt.jpg"
 
     # カメラ異常検知
-    camera_exception(img)
+    if camera_exception(image_path):
+        print("画像に異常があります。行列が検知できません。")
+    else:
+        print("画像に異常はありません。行列を検知します。")
 
-    # 画像をグレースケール化
-    image_name = gray_scale(image_name, img)
-    img = cv2.imread(image_name)
+        # 画像をグレースケール化
+        image_path = gray_scale(image_path)
 
-    # 画像をガウスぼかし化
-    image_name = gauss_blur(image_name, img)
-    img = cv2.imread(image_name)
+        # 画像をガウスぼかし化
+        image_path = gauss_blur(image_path)
 
-    # 画像を二値化
-    binarization(image_name, img)
+        # 画像を二値化
+        binarization(image_path)
 
 
 # カメラの異常検知
-def camera_exception(img):
+def camera_exception(image_path):
+
+    """
+    camera_exception : カメラの異常検知
+    Input   : 画像パス
+    Output  : 0 (正常な場合) or 1 (異常な場合)
+    
+    argument
+
+    image_path(string)  : 画像パス
+    """
+
+    # 異常な場合
+    if brightness_check(image_path):
+        print("輝度")
+        return 1
+    
+    elif resolution_check(image_path, 1944, 2592):
+        print("解像度")
+        return 1
+
+    # 正常な場合
+    else:
+        return 0
+
+
+# 輝度チェック
+def brightness_check(image_path):
+
+    """
+    brightness_check : 輝度チェック
+    Input   : 画像パス
+    Output  : 0 (正常な場合) or 1 (異常な場合)
+
+    argument
+
+    image_path  : 画像パス
+    """
+
+    img = cv2.imread(image_path)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
 
@@ -43,10 +82,49 @@ def camera_exception(img):
         return 0
 
 
-# グレースケール画像を保存
-def gray_scale(image_name, img):
+# 解像度チェック
+def resolution_check(image_path, check_height, check_width):
 
-    gray_image_name = "./img/gray_" + image_name[6:]
+    """
+    brightness_check : 解像度チェック
+    Input   : 画像パス
+    Output  : 0 (正常な場合) or 1 (異常な場合)
+
+    argument
+
+    image_path  : 画像パス
+    check_height    : チェックする画像の高さ
+    check_width     : チェックする画像の幅
+    """
+
+    img = cv2.imread(image_path)
+    height, width = img.shape[:2]
+
+    # 異常の場合
+    if height != check_height or width != check_width:
+        return 1
+    
+    # 正常の場合
+    else:
+        return 0
+
+
+# グレースケール画像を保存
+def gray_scale(image_path):
+
+    """
+    gray_scale : グレースケール画像を保存
+    Input   : 画像パス
+    Output  : グレースケール画像
+
+    argument
+
+    image_path  : 画像パス
+    """
+
+    img = cv2.imread(image_path)
+
+    gray_image_name = "./img/gray_" + image_path[6:]
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(gray_image_name, gray_image)
 
@@ -54,9 +132,21 @@ def gray_scale(image_name, img):
 
 
 # ノイズ除去
-def gauss_blur(image_name, img):
+def gauss_blur(image_path):
 
-    gauss_image_name = "./img/gauss_" + image_name[6:]
+    """
+    gauss_blur : ノイズ除去後の画像を保存
+    Input   : 画像パス
+    Output  : ノイズ除去後の画像
+
+    argument
+
+    image_path  : 画像パス
+    """
+
+    img = cv2.imread(image_path)
+
+    gauss_image_name = "./img/gauss_" + image_path[6:]
     gauss_image = cv2.GaussianBlur(img, (17, 17), 11)
     cv2.imwrite(gauss_image_name, gauss_image)
 
@@ -64,9 +154,21 @@ def gauss_blur(image_name, img):
 
 
 # 二値化
-def binarization(image_name, img):
+def binarization(image_path):
 
-    binary_image_name = "./img/binary_" + image_name[6:]
+    """
+    gray_scale : 二値化画像を保存
+    Input   : 画像パス
+    Output  : 二値化画像(./img/queue_detected.jpg)
+
+    argument
+
+    image_path  : 画像パス
+    """
+
+    img = cv2.imread(image_path)
+
+    binary_image_name = "./img/queue_detected.jpg"
     ret, binary_image = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
     cv2.imwrite(binary_image_name, binary_image)
 
